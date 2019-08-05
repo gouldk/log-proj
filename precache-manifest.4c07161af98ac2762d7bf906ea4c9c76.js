a51c4d94a5928c9bd0340bca62934a548caa982a
gouldk <gouldk4@gmail.com>
2019-08-05 15:46:18 -0400

    Updates

--- precache-manifest.4c07161af98ac2762d7bf906ea4c9c76.js
+++ precache-manifest.4c07161af98ac2762d7bf906ea4c9c76.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c08bac666b3ff54f489ed55cccbeaff",
+    "revision": "0bb3c819012563f109d2d6a3be25d8be",
     "url": "/log-proj/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/log-proj/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "9fdf85ffca37d35f9861",
-    "url": "/log-proj/static/css/main.23721cc0.chunk.css"
+    "revision": "5567fb6c84b2c55c33cb",
+    "url": "/log-proj/static/css/main.54abca3b.chunk.css"
   },
   {
     "revision": "af9e529f3b4581309311",
     "url": "/log-proj/static/js/2.2283838c.chunk.js"
   },
   {
-    "revision": "9fdf85ffca37d35f9861",
-    "url": "/log-proj/static/js/main.38c67e97.chunk.js"
+    "revision": "5567fb6c84b2c55c33cb",
+    "url": "/log-proj/static/js/main.5ed84ee8.chunk.js"
   },
   {
     "revision": "161fb7c772108733401b",
